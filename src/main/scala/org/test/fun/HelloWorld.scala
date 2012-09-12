6b8d437e84435622d3d100a5b95436f86aeb1991
Kazi Abdullah Saikat <saikat@viatravel.no>
2012-09-12 14:46:58 +0200

    Check-in the first files.

---
+++ src/main/scala/org/test/fun/HelloWorld.scala
@@ -0,0 +1,21 @@
+package org.test.fun
+
+import org.joda.time.DateTime
+import scala.Console
+
+object HelloWorld {
+	def main(argv: Array[String]) {
+	  println("My Lord, your wish is my command!")
+	  val dateTime = DateTime.now()
+	  println("Local time: " + dateTime.toString("dd-MM-yyyy kk:mm:ss"))
+	  while (true) {
+		  val command = Console.readLine();
+		  println("My lord wishes: " + command)
+		  command match {
+		    case "ls" => println("My Lord, can't show directory listing!")
+		    case "rm" => println("My Lord, removal is not possible!")
+		    case _ => println("Servent is unable to understand your command!")
+		  }
+	  }
+	}
+}
