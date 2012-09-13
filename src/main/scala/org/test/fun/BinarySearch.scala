77c41932f6a507aef91168db270b62fa95a3f97a
Kazi Abdullah Saikat <saikat@viatravel.no>
2012-09-13 11:28:34 +0200

    Code clean-up

--- src/main/scala/org/test/fun/BinarySearch.scala
+++ src/main/scala/org/test/fun/BinarySearch.scala
@@ -2,16 +2,14 @@ package org.test.fun
 
 object BinarySearch {
 	def main(argv: Array[String]) = {
-	  if (argv.length == 0) { println("usage: program number"); System.exit(-1) }
+	  if (argv.length == 0) { 
+	    println("usage: program number"); 
+	    System.exit(-1) 
+	  }
 	  val values = Array(10, 11, 15, 20, 22, 29, 35, 50)
-	  val target = argv(0).toInt;
-	  var left: Int = 0;
-	  var right: Int = values.length - 1;
-	  
-	  var found: Boolean = false;
-	  var index: Int = binarySearch(0, values.length - 1, values, target)
+	  val index = binarySearch(0, values.length - 1, values, argv(0).toInt)
 	  if (index >= 0) {
-	    println("found number: " + target + " at position: " + index)
+	    println("found number at position: " + index)
 	  } else {
 	    println("Not found")
 	  }
