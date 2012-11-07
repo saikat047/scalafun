089414ad191b1417afbc3b1bc2662e5c5e0f05de
Kazi Abdullah Saikat <saikat@viatravel.no>
2012-11-07 14:32:13 +0100

    Add "Seriously" strange scala syntax in main method

--- src/main/scala/org/test/fun/lang/StrangeScala.scala
+++ src/main/scala/org/test/fun/lang/StrangeScala.scala
@@ -4,23 +4,41 @@ object StrangeScala {
 
   def main(argv : Array[String]) {
     you make it print "saikat"
+    you suck dude
+    you are great
   }
 
+  def stranger() = new Stranger
+
   def you() : Stranger = {
-    print("i am ")
+    print("i ")
     new Stranger
   }
 
   def it() : It = new It
 
+  def great() = new Great
+
+  def dude : Dude = new Dude
+
   class Stranger {
     def make(it : It) : Stranger = {
-      scala.Predef.print("making it ")
+      scala.Predef.print("am making it ")
       this
     }
-    def print(message : String) = scala.Predef.print("print " + message)
+
+    def print(message : String) = scala.Predef.println("print '" + message + "'.")
+
+    def suck(dude : Dude) = scala.Predef.println("suck!!!")
+
+    def are(g : Great) {
+      scala.Predef.println("am great.")
+    }
   }
 
-  class It {
-  }
+  class It {}
+
+  class Great {}
+
+  class Dude {}
 }
